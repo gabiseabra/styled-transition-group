303160fcab6a518b859dadcb4ac22d4be001e748
Gabriela Seabra <gabriela.lima.se@gmail.com>
2017-12-15 19:55:28 -0200

    Pass props to transition

--- src/AnimatedComponent.jsx
+++ src/AnimatedComponent.jsx
@@ -2,21 +2,32 @@ import React from "react"
 import PropTypes from "prop-types"
 import CSSTransition from "react-transition-group/CSSTransition"
 
-const AnimatedComponent = ({ tag: Tag, transition, in: active, classNames, ...props }) => {
+const transitionPropTypes = { ...CSSTransition.propTypes }
+delete transitionPropTypes.children
+
+const groupProps = (props) => {
+  const groups = [ [], [] ]
+  Object.entries(props).forEach(([ val, key ]) => {
+    if((key in transitionPropTypes)) {
+      groups[0].push(val)
+    } else {
+      groups[1].push(val)
+    }
+  })
+  return groups
+}
+const AnimatedComponent = ({ tag: Tag, ...props }) => {
+  const [ transition, rest ] = groupProps(props)
   return (
-    <CSSTransition {...transition} classNames={classNames} in={active}>
-      <Tag {...props} />
+    <CSSTransition {...transition}>
+      <Tag {...rest} />
     </CSSTransition>
   )
 }
 
 AnimatedComponent.propTypes = {
-  tag: PropTypes.oneOfType([ PropTypes.func, PropTypes.string ]).isRequired,
-  in: PropTypes.bool.isRequired,
-  transition: PropTypes.object.isRequired,
-  classNames: PropTypes.objectOf(PropTypes.string).isRequired
+  tag: PropTypes.oneOfType([ PropTypes.func, PropTypes.string ]).isRequired
 }
-
 
 export default AnimatedComponent
 
