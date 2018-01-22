30e145601a82fdcd08ad6022415b4f5d7474b7ad
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-22 20:03:06 -0200

    Added invalid target error

--- src/css.js
+++ src/css.js
@@ -5,10 +5,10 @@ const pseudoElements = Object.values(STATES)
 const pattern = new RegExp(`([^\\s;}]+|^):(${pseudoElements.join("|")}(?=\\s*[{,]))`, "g")
 
 export default function parseCss(strings, ...interpolations) {
-  const result = strings.map((body, i) => body.replace(pattern, (_, target, state) => {
+  const result = strings.map(body => body.replace(pattern, (_, target, state) => {
     if(target === "&") return `&-${state}`
     // TODO
-    console.log(target)
+    throw new Error(`Invalid transition target ${target}`)
   }))
   return [ result, ...interpolations ]
 }
