7507f02d08872ca0dafe213390b3d23fd02952e9
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-02-20 21:50:50 -0300

    Fix constructor config separation

--- src/construct/index.js
+++ src/construct/index.js
@@ -2,30 +2,34 @@ import styled from "styled-components"
 import groupProps from "../AnimatedComponent/props"
 import css from "./css"
 
-const CONSTRUCTOR_METHODS = [ "withConfig", "attrs" ]
-
 const delegate = animated => fun => (...props) => animated(fun`${css(...props)}`)
 
+const groupConfig = (config) => {
+  const groups = { transition: {}, styled: { ...config } }
+  if(config.attrs) {
+    const { transition, props } = groupProps(config.attrs)
+    groups.transition.attrs = transition
+    groups.styled.attrs = props
+  }
+  return groups
+}
+
 export const extend = (animated, target) => {
-  const delegateThis = delegate(animated)
-  const result = delegateThis(target)
-  CONSTRUCTOR_METHODS.forEach((method) => {
-    result[method] = (...args) => delegateThis(target[method](...args))
-  })
+  const delegateThis = cfg => delegate(animated(cfg))
+  const result = delegateThis({})(target)
+  result.withConfig = (config) => {
+    const cfg = groupConfig(config)
+    return delegateThis(cfg.transition)(target.withConfig(cfg.styled))
+  }
+  result.attrs = (attrs) => {
+    const cfg = groupConfig({ attrs })
+    return delegateThis(cfg.transition)(target.attrs(cfg.styled.attrs))
+  }
   return result
 }
 
 export default (animated, Tag, config) => {
-  const transitionConfig = { }
   let target = styled(Tag)
-  if(config) {
-    const styledConfig = { ...config }
-    if(config.attrs) {
-      const props = groupProps(config.attrs)
-      styledConfig.attrs = props.props
-      transitionConfig.attrs = props.transition
-    }
-    target = target.withConfig(styledConfig)
-  }
-  return extend(animated(transitionConfig), target)
+  if(config) target = target.withConfig(config)
+  return extend(animated, target)
 }
