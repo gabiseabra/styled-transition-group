77d06704da1361ca05053f6b3be9dd47ec60605b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-02-20 15:24:08 -0300

    fix timeout not being passed to CSSTransition

--- src/AnimatedComponent/props.js
+++ src/AnimatedComponent/props.js
@@ -1,7 +1,5 @@
 const transitionProps = [
   "in",
-  // Pass timeout through to use in css templates
-  // "timeout",
   "mountOnEnter",
   "unmountOnExit",
   "onEnter",
@@ -16,10 +14,25 @@ const transitionProps = [
   "onAppear"
 ]
 
+const bothProps = [
+  // Pass timeout through to use in css templates
+  "timeout"
+]
+
+/**
+ * Select which props to pass to transition and child component.
+ * @param {Object} props               All props
+ * @returns {Object} groups
+ * @returns {Object} groups.transition Transition props
+ * @returns {Object} groups.props      Child component props
+ */
 export default function partition(props) {
   const groups = { transition: {}, props: {} }
   Object.entries(props).forEach(([ key, value ]) => {
-    if(transitionProps.includes(key)) {
+    if(bothProps.includes(key)) {
+      groups.transition[key] = value
+      groups.props[key] = value
+    } else if(transitionProps.includes(key)) {
       groups.transition[key] = value
     } else {
       groups.props[key] = value
